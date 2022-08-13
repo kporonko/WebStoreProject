adc8dc08498c47f2b7fc22d0d43056e066901dd0
KeeYoo <poronko2045@gmail.com>
2022-08-13 21:17:24 +0300

    Controller is ready. Fixed bugs with lazy loading

---
+++ WebStoreApi/WebStoreApi/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebStoreApi.Data;
+using WebStoreApi.ModelsEF;
+
+namespace WebStoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet("getAllProducts")]
+        public List<Product> GetProducts()
+        {
+            List<Product> products = ContextProvider.db.Products.ToList();
+            return products;
+        }
+
+        [HttpGet("prodById")]
+        public Product ProductById(int id)
+        {
+            Product product = ContextProvider.db.Products.ToList().First(p => p.ProductId == id);
+            return product;
+        }
+    }
+}
--- WebStoreApi/WebStoreApi/Program.cs
+++ WebStoreApi/WebStoreApi/Program.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers().AddJsonOptions(x =>
+                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 var app = builder.Build();
 
